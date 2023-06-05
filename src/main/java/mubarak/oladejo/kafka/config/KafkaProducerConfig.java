d3145ecd177021e15d6cc78528b3dcd934a9b733
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-05 15:15:40 +0100

    downloading dependency and configuring Kafka

--- src/main/java/mubarak/oladejo/kafka/config/KafkaProducerConfig.java
+++ src/main/java/mubarak/oladejo/kafka/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package mubarak.oladejo.kafka.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.serializer.Serializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,8 +16,8 @@ public class KafkaProducerConfig {
     public Map<String, Object> createProducerConfig(){
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Serializer.class);
         return props;
     }
 }
