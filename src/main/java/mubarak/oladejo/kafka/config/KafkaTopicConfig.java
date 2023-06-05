bf2cb215a73679dea9c52020b3686deef263f14d
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-05 15:03:34 +0100

    downloading dependency and configuring Kafka

--- src/main/java/mubarak/oladejo/kafka/config/KafkaTopicConfig.java
+++ src/main/java/mubarak/oladejo/kafka/config/KafkaTopicConfig.java
@@ -1,12 +1,14 @@
 package mubarak.oladejo.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
 
+    @Bean
     public NewTopic createKafkaTopic(){
         return TopicBuilder.name("mubaraktopic")
                 .build();
