2aa05e0172ba76123f2da718f3d264d2660d7f2d
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-08 10:25:33 +0100

    addig message broker registry

--- src/main/java/mubarak/oladejo/kafka/MessageController.java
+++ src/main/java/mubarak/oladejo/kafka/MessageController.java
@@ -1,12 +1,23 @@
 package mubarak.oladejo.kafka;
 
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/messages")
 public class MessageController {
-    public void publish(){
+    private KafkaTemplate<String, String> kafkaTemplate;
 
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request){
+        kafkaTemplate.send("mubaraktopic", request.message());
     }
 }
